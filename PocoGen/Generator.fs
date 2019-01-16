2840d0deec0067128a4d0fdc51939e7174304474
Terrance Smith <Terrance.Smith@cdpehs.com>
2019-01-16 14:01:14 -0500

    init

---
+++ PocoGen/Generator.fs
@@ -0,0 +1,26 @@
+﻿module Generator
+
+open System.Collections.Generic
+open System
+open System.Data
+open System.Text
+
+
+//let TypeAliases = new Dictionary<Type, string> ()
+let DumpCSharpClass(connection:IDbConnection) (sql:string) (className: string) :string =
+
+    let buildClassFromTable =
+        CsharpFormatting.buildClass className
+
+    connection.Open()
+    let cmd = connection.CreateCommand()
+    cmd.CommandText <- sql
+    let reader = cmd.ExecuteReader()
+    let sb = new StringBuilder()
+
+    let resultClass=
+        reader.GetSchemaTable().Select()
+        |> buildClassFromTable
+
+    connection.Close()
+    resultClass
